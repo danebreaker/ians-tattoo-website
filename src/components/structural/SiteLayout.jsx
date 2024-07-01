22fed705dc561fb92b2659616a11d1fc29e002f6
Highground-ai <ro.1.babaria@gmail.com>
2024-07-01 15:29:29 -0500

    Update SiteLayout.jsx

--- src/components/structural/SiteLayout.jsx
+++ src/components/structural/SiteLayout.jsx
@@ -1,14 +1,17 @@
-import { Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
 
 export default function SiteLayout(props) {
     return <div>
-        <Navbar>
-            <Nav>
+        <Navbar fixed="top" bg="dark" data-bs-theme="dark">
+            <Container>
+            <Nav className="me-auto">
                 <Nav.Link as={Link} to="./SiteHome">Home</Nav.Link>
                 <Nav.Link as={Link} to="./AboutMe">About Me</Nav.Link>
             </Nav>
+            </Container>
         </Navbar>
+        
         <div style={{ margin: "1rem" }}>
             <Outlet />
         </div>
