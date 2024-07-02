c93fc06f0c5ac42dc8659459ebfd2e93bd06a22b
Highground-ai <ro.1.babaria@gmail.com>
2024-07-01 22:29:20 -0500

    Monkey Width

--- src/components/content/SiteHome.jsx
+++ src/components/content/SiteHome.jsx
@@ -1,7 +1,12 @@
-import React from "react"
+import React from "react";
+import {Container, Image} from "react-bootstrap";
 
 export default function SiteHome() {
-    return <>
-        <h1>Home</h1>
-    </>
+    return (
+    <>
+        <Container max-width='100%'>
+        <Image src="src/images/DemoBanner.jpg" fluid ></Image>
+        </Container>
+
+    </>)
 }
